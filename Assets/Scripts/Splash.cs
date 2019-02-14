959da81a7c190f8b043338063eb869ad62a0c408
Josh <josh@vrunlimited.com.au>
2019-02-14 17:33:44 +1100

    Lecture 81

---
+++ Assets/Scripts/Splash.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Splash : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadLevel", 3f);
+
+    }
+
+    private void LoadLevel()
+    {
+        print("LoadLevel");
+        SceneManager.LoadScene(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+    }
+}
